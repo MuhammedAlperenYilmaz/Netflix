7e5de0e8444d5484a930a75092f0942384e62a68
MuhammedAlperenYilmaz <alprnylmz_06@hotmail.com>
2024-04-22 11:32:41 +0300

    created a new class of main. Created aa new methood which name is runToNeflix

---
+++ src/test/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Netflix netflix = new Netflix();
+        netflix.runToNetflix();
+    }
+}
--- src/test/java/netflix.java
+++ src/test/java/netflix.java
@@ -4,47 +4,16 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
-public class netflix {
-    private static final String email_Id = ":r0:"; //okuması kolay olsun diye ve değişiklik yapılamasın diye
-    private static final String password_Id = ":r3:";
-    private static final String[] Ids = {"onetrust-reject-all-handler","signIn", email_Id, password_Id};
-    private static final String email = "test@hotmail.com";
-    private static final String password = "123456";
+public class Netflix {
+    private final String email_Id = ":r0:"; // okuması kolay olsun diye ve değişiklik yapılamasın diye
+    private final String password_Id = ":r3:";
+    private final String[] Ids = { "onetrust-reject-all-handler", "signIn", email_Id, password_Id };
+    private final String email = "test@hotmail.com";
+    private final String password = "123456";
 
-    public static void initializedDriver() {
-        WebDriverManager.chromedriver();
-        WebDriverManager.chromedriver().setup();
-    }
-
-    public static void navigateToSite(WebDriver driver) {
-        driver.get("https://www.netflix.com/tr/");
-    }
-
-    public static void handleIds(WebDriver driver) throws InterruptedException {
-        for (String Id : Ids) {
-            WebElement element = driver.findElement(By.id(Id));
-            if (Id.equals(email_Id)) {
-                enterText(element, email);
-            } else if (Id.equals(password_Id)) {
-                enterText(element, password_Id);
-            } else {
-                element.click();
-            }
-        }
-    }
-
-    public static void enterText(WebElement element, String text) throws InterruptedException {
-        element.sendKeys(text);
-        element.sendKeys(Keys.ENTER);
-    }
-
-    public static void main(String[] args) {
+    public void runToNetflix() {
         initializedDriver();
         WebDriver driver = new ChromeDriver();
 
@@ -62,4 +31,32 @@ public class netflix {
         }
 
     }
+
+    private void initializedDriver() {
+        WebDriverManager.chromedriver();
+        WebDriverManager.chromedriver().setup();
+    }
+
+    private void navigateToSite(WebDriver driver) {
+        driver.get("https://www.netflix.com/tr/");
+    }
+
+    private void handleIds(WebDriver driver) throws InterruptedException {
+        for (String Id : Ids) {
+            WebElement element = driver.findElement(By.id(Id));
+            if (Id.equals(email_Id)) {
+                enterText(element, email);
+            } else if (Id.equals(password_Id)) {
+                enterText(element, password_Id);
+            } else {
+                element.click();
+            }
+        }
+    }
+
+    private void enterText(WebElement element, String text) throws InterruptedException {
+        element.sendKeys(text);
+        element.sendKeys(Keys.ENTER);
+    }
+
 }
